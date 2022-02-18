a950046693305d9cdbc00d0fc3a54c4fc03a0240
Sára Mustre-del Río <saramr09@uw.edu>
2022-02-18 14:56:39 -0800

    ass intructions

--- Summary-info.R
+++ Summary-info.R
@@ -1,1 +1,14 @@
 project_data<-read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-sara-mustredr09/main/Juvenile_Justice_Dashboard_-_HS_Completion.csv?token=GHSAT0AAAAAABQIEWSA6BKJCPJ3VN3C5VHSYQYGLCA")
+
+# 1. Which word (hs outcome) pops up the most for each race?
+ # Functions to use: summarize ( mode?)
+#Groupby by demographic value
+# 2. Which word (offender type) pops up the most for each race?
+ # Functions to use: summarize ( mode?)
+# Groupby by demographic value
+# 3. Average percentage of not justice involved per demographic value
+#RedactedPct
+# 4. Average percentage of justice involved (justice involved, juvenile offender, status offender) per demographic value
+#RedactedPct
+# 5. Proportion of dropouts w/justice involvement
+# 7. Proportion of not justice involved w/HS Diploma
