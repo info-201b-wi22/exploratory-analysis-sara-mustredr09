cda0f81ca0d23a0b1c6f82549c11f895d40a3828
amayalw <amayalw@uw.edu>
2022-02-21 20:25:43 -0800

    add table and chart 1

--- Chart1.R
+++ Chart1.R
@@ -1,1 +1,22 @@
 project_data<-read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-sara-mustredr09/main/Juvenile_Justice_Dashboard_-_HS_Completion.csv?token=GHSAT0AAAAAABQIEWSA6BKJCPJ3VN3C5VHSYQYGLCA")
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+# A dodged bar chart showing the high school outcomes for each offender type
+# (justice-involved, not justice-involved, juvenile offender, status offender)
+avg_pct_offenders <- project_data %>%
+  filter(DemographicGroup == "All Students") %>% 
+  filter(DemographicValue == "All Students")
+
+pct_dropout <- project_data %>% 
+  group_by(JJOffenderType) %>% 
+  filter(HSOutcome == "Dropout") %>% 
+  summarise(avg_pct_dropout = mean(Pct, na.rm = TRUE))
+  
+ggplot(data = avg_pct_offenders) +
+  geom_col(
+    mapping = aes(x = JJOffenderType, y = RedactedPct, fill = HSOutcome), position = "dodge"
+  )
+
--- Table.R
+++ Table.R
@@ -1,1 +1,13 @@
 project_data<-read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-sara-mustredr09/main/Juvenile_Justice_Dashboard_-_HS_Completion.csv?token=GHSAT0AAAAAABQIEWSA6BKJCPJ3VN3C5VHSYQYGLCA")
+
+library(tidyr)
+library(tidyverse)
+hs_outcome_table <- project_data %>% 
+  group_by(CohortYearTTL) %>% 
+  filter(DemographicGroup == "Race/Ethnicity") %>% 
+  filter(JJOffenderType == "Not Justice Involved") %>% 
+  filter(HSOutcome == "HS Diploma") %>% 
+  rename("Pct_Not_Justice_Involved" = "Pct") %>% 
+  select(-c(DemographicGroup, JJOffenderType, RedactedPct)) %>% 
+  rename("Race/Ethnicity" = "DemographicValue")
+ 
