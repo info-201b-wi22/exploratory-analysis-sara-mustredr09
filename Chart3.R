c0be76d23632dfa6af99e900a5dc2ff7f417b68c
Sára Mustre-del Río <saramr09@uw.edu>
2022-02-21 23:40:08 -0800

    add chart 3

--- Chart3.R
+++ Chart3.R
@@ -1,1 +1,20 @@
 project_data<-read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-sara-mustredr09/main/Juvenile_Justice_Dashboard_-_HS_Completion.csv?token=GHSAT0AAAAAABQIEWSA6BKJCPJ3VN3C5VHSYQYGLCA")
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+#Scatterplot representing Dropout rate per Justice Involvement category.
+
+pct_dropouts <- project_data %>% 
+  group_by(JJOffenderType) %>% 
+  filter(HSOutcome == "Dropout") %>% 
+  filter(CohortYearTTL > 2016) %>% 
+  summarise(avg_pct_dropouts = mean(Pct, na.rm = TRUE))
+  
+ggplot(data = pct_dropouts) +
+  geom_point(
+    mapping = aes(x = JJOffenderType, y = avg_pct_dropouts, color = JJOffenderType)
+  ) 
+
+ggplot(pct_dropouts) +
+  geom_col(mapping = aes(x =JJOffenderType , y = avg_pct_dropouts, fill = JJOffenderType))
